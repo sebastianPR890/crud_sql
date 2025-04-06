156bf84dd67a041632f9a5f9e37cd68b0b5e49d6
SebastianPR890 <sebitasperirodri850@gmail.com>
2025-04-06 11:53:40 -0500

    configuracion de aplicacion e interfaz

--- src/main/java/com/mycompany/crud_sql/CAlumnos.java
+++ src/main/java/com/mycompany/crud_sql/CAlumnos.java
@@ -1,16 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.crud_sql;
 
 /**
  *
  * @author sebit
  */
-public class Crud_sql {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public class CAlumnos {
+    
 }
---
+++ src/main/java/com/mycompany/crud_sql/inicio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.crud_sql;
+
+/**
+ *
+ * @author sebit
+ */
+public class inicio {
+    public static void main(String[] args) {
+        
+    }
+    
+}
