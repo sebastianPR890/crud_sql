ed4c51410616fa99ba60a5b6acce5fd5b7533a31
SebastianPR890 <sebitasperirodri850@gmail.com>
2025-04-06 11:16:46 -0500

    Inicio del proyecto

---
+++ src/main/java/com/mycompany/crud_sql/Crud_sql.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crud_sql;
+
+/**
+ *
+ * @author sebit
+ */
+public class Crud_sql {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
